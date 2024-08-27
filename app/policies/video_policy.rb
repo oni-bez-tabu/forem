f6c699dd66ea9fc21f7a8783e783e1ac360dc9d4
Oni Bez Tabu <oni.bez.tabu@gmail.com>
2024-08-27 09:32:05 +0000

    video policy hack

--- app/policies/video_policy.rb
+++ app/policies/video_policy.rb
@@ -8,6 +8,8 @@ class VideoPolicy < ApplicationPolicy
   #       what that means.  [@jeremyf] envisions that we could use an admin setting to allow the
   #       administrators to set the number of days to consider a "new user".
   def create?
+    return true
+
     return false unless Settings::General.enable_video_upload
 
     require_user_in_good_standing!
@@ -16,7 +18,7 @@ class VideoPolicy < ApplicationPolicy
     # recently?"
     return user_any_admin? if ArticlePolicy.limit_post_creation_to_admins?
 
-    user.created_at.before?(1.hour.ago)
+    user.created_at.before?(7.days.ago)
   end
 
   alias new? create?
